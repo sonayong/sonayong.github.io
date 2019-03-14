d1b82ec8adca0ed5c0a036d21743ef7aab066969
sonayong <48547591+sonayong@users.noreply.github.com>
2019-03-14 17:27:53 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,90 @@
+
+$(document).ready(function(){
+
+$('#tab-week1').click(function(){
+    $('.tab').css('background','rgba(231, 234, 237, 1)').css('border','0px').css('border-bottom','1px solid #ccc').css('border-radius','0px').css('z-index','1');
+    $('#tab-week1').css('background','#fff').css('border','1px solid #ccc').css('border-bottom','0px').css('border-radius','10px 10px 0px 0px').css('z-index','2');
+    $('.content').hide();
+    $('#content-week1').show();
+});
+
+$('#tab-week2').click(function(){
+    $('.tab').css('background','rgba(231, 234, 237, 1)').css('border','0px').css('border-bottom','1px solid #ccc').css('border-radius','0px').css('z-index','1');
+    $('#tab-week2').css('background','#fff').css('border','1px solid #ccc').css('border-bottom','0px').css('border-radius','10px 10px 0px 0px').css('z-index','2');
+    $('.content').hide();
+    $('#content-week2').show();
+});
+
+$('#tab-week3').click(function(){
+    $('.tab').css('background','rgba(231, 234, 237, 1)').css('border','0px').css('border-bottom','1px solid #ccc').css('border-radius','0px').css('z-index','1');
+    $('#tab-week3').css('background','#fff').css('border','1px solid #ccc').css('border-bottom','0px').css('border-radius','10px 10px 0px 0px').css('z-index','2');
+    $('.content').hide();
+    $('#content-week3').show();
+});
+
+$('#tab-week4').click(function(){
+    $('.tab').css('background','rgba(231, 234, 237, 1)').css('border','0px').css('border-bottom','1px solid #ccc').css('border-radius','0px').css('z-index','1');
+    $('#tab-week4').css('background','#fff').css('border','1px solid #ccc').css('border-bottom','0px').css('border-radius','10px 10px 0px 0px').css('z-index','2');
+    $('.content').hide();
+    $('#content-week4').show();
+});
+
+$('#tab-week5').click(function(){
+    $('.tab').css('background','rgba(231, 234, 237, 1)').css('border','0px').css('border-bottom','1px solid #ccc').css('border-radius','0px').css('z-index','1');
+    $('#tab-week5').css('background','#fff').css('border','1px solid #ccc').css('border-bottom','0px').css('border-radius','10px 10px 0px 0px').css('z-index','2');
+    $('.content').hide();
+    $('#content-week5').show();
+});
+
+$('#tab-week6').click(function(){
+    $('.tab').css('background','rgba(231, 234, 237, 1)').css('border','0px').css('border-bottom','1px solid #ccc').css('border-radius','0px').css('z-index','1');
+    $('#tab-week6').css('background','#fff').css('border','1px solid #ccc').css('border-bottom','0px').css('border-radius','10px 10px 0px 0px').css('z-index','2');
+    $('.content').hide();
+    $('#content-week6').show();
+});
+
+$('#tab-week7').click(function(){
+    $('.tab').css('background','rgba(231, 234, 237, 1)').css('border','0px').css('border-bottom','1px solid #ccc').css('border-radius','0px').css('z-index','1');
+    $('#tab-week7').css('background','#fff').css('border','1px solid #ccc').css('border-bottom','0px').css('border-radius','10px 10px 0px 0px').css('z-index','2');
+    $('.content').hide();
+    $('#content-week7').show();
+});
+
+$('#tab-week8').click(function(){
+    $('.tab').css('background','rgba(231, 234, 237, 1)').css('border','0px').css('border-bottom','1px solid #ccc').css('border-radius','0px').css('z-index','1');
+    $('#tab-week8').css('background','#fff').css('border','1px solid #ccc').css('border-bottom','0px').css('border-radius','10px 10px 0px 0px').css('z-index','2');
+    $('.content').hide();
+    $('#content-week8').show();
+});
+
+$('#tab-week9').click(function(){
+    $('.tab').css('background','rgba(231, 234, 237, 1)').css('border','0px').css('border-bottom','1px solid #ccc').css('border-radius','0px').css('z-index','1');
+    $('#tab-week9').css('background','#fff').css('border','1px solid #ccc').css('border-bottom','0px').css('border-radius','10px 10px 0px 0px').css('z-index','2');
+    $('.content').hide();
+    $('#content-week9').show();
+});
+
+$('#tab-week10').click(function(){
+    $('.tab').css('background','rgba(231, 234, 237, 1)').css('border','0px').css('border-bottom','1px solid #ccc').css('border-radius','0px').css('z-index','1');
+    $('#tab-week10').css('background','#fff').css('border','1px solid #ccc').css('border-bottom','0px').css('border-radius','10px 10px 0px 0px').css('z-index','2');
+    $('.content').hide();
+    $('#content-week10').show();
+});
+
+$('#tab-week11').click(function(){
+    $('.tab').css('background','rgba(231, 234, 237, 1)').css('border','0px').css('border-bottom','1px solid #ccc').css('border-radius','0px').css('z-index','1');
+    $('#tab-week11').css('background','#fff').css('border','1px solid #ccc').css('border-bottom','0px').css('border-radius','10px 10px 0px 0px').css('z-index','2');
+    $('.content').hide();
+    $('#content-week11').show();
+});
+
+$('#tab-week12').click(function(){
+    $('.tab').css('background','rgba(231, 234, 237, 1)').css('border','0px').css('border-bottom','1px solid #ccc').css('border-radius','0px').css('z-index','1');
+    $('#tab-week12').css('background','#fff').css('border','1px solid #ccc').css('border-bottom','0px').css('border-radius','10px 10px 0px 0px').css('z-index','2');
+    $('.content').hide();
+    $('#content-week12').show();
+});
+
+
+
+});
